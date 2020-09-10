4dd2fa87b212e46738df0a7baf21234a651cca53
MattW <mattnwarnock@yahoo.co.uk>
2020-09-10 15:03:20 +0100

    Test for user input collection and refactor

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -5,6 +5,8 @@ class UserInterface
   MENU_MESSAGE = "\n---------------------\n\n\s\s\s\sCONTACT BOOK\n\n---------------------\n
   \n1) Exit the program\n\nChoose a menu option: "
 
+  attr_reader :user_input
+
   def initialize(input, output)
     @input = input
     @output = output
@@ -13,6 +15,7 @@ class UserInterface
   def run
     output.write CLEAR_COMMAND
     output.print MENU_MESSAGE
+    @user_input = input.gets.chomp
   end
 
   private
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -3,9 +3,11 @@
 require 'user_interface'
 
 RSpec.describe UserInterface do
+  let(:valid_input) { StringIO.new('1') }
+  let(:output) { StringIO.new }
+
   it 'prints menu of options for user to choose' do
-    output = StringIO.new
-    ui = described_class.new(nil, output)
+    ui = described_class.new(valid_input, output)
 
     ui.run
 
@@ -13,11 +15,18 @@ RSpec.describe UserInterface do
   end
 
   it 'Clears the screen before printing the menu' do
-    output = StringIO.new
-    ui = described_class.new(nil, output)
+    ui = described_class.new(valid_input, output)
 
     ui.run
 
     expect(output.string).to include("\033[H\033[2J")
+  end
+
+  it 'reads an input from the user' do
+    ui = described_class.new(valid_input, output)
+
+    ui.run
+
+    expect(ui.user_input).to eq('1')
   end
 end
