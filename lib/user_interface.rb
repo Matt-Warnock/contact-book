9f53f9bb7aa638980dbea4229edc30f712643524
MattW <mattnwarnock@yahoo.co.uk>
2020-09-10 17:56:07 +0100

    Add tests for vaidations and second input collection

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -2,6 +2,7 @@
 
 class UserInterface
   CLEAR_COMMAND = "\033[H\033[2J"
+  ERROR_MESSAGE = 'Invalid input'
   MENU_MESSAGE = "\n---------------------\n\n\s\s\s\sCONTACT BOOK\n\n---------------------\n
   \n1) Exit the program\n\nChoose a menu option: "
 
@@ -15,10 +16,21 @@ class UserInterface
   def run
     output.write CLEAR_COMMAND
     output.print MENU_MESSAGE
+
     @user_input = input.gets.chomp
+
+    if valid?(user_input)
+      user_input
+    else
+      output.puts ERROR_MESSAGE
+    end
   end
 
   private
+
+  def valid?(option)
+    option.match?(/^\d$/) && option == '1'
+  end
 
   attr_reader :input, :output
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -3,8 +3,8 @@
 require 'user_interface'
 
 RSpec.describe UserInterface do
-  let(:valid_input) { StringIO.new('1') }
   let(:output) { StringIO.new }
+  let(:valid_input) { StringIO.new('1') }
 
   it 'prints menu of options for user to choose' do
     ui = described_class.new(valid_input, output)
@@ -28,5 +28,40 @@ RSpec.describe UserInterface do
     ui.run
 
     expect(ui.user_input).to eq('1')
+  end
+
+  it 'validates input is a number' do
+    input = StringIO.new('a')
+    ui = described_class.new(input, output)
+
+    ui.run
+
+    expect(output.string).to include(described_class::ERROR_MESSAGE)
+  end
+
+  it 'validates input only if the number is 1' do
+    input = StringIO.new('2')
+    ui = described_class.new(input, output)
+
+    ui.run
+
+    expect(output.string).to include(described_class::ERROR_MESSAGE)
+  end
+
+  it 'reads the input again if input is invalid' do
+    input = StringIO.new('2')
+    ui = described_class.new(input, output)
+
+    ui.run
+    input.string = '1'
+
+    expect(ui.user_input).to eq('1')
+  end
+
+  it 'returns a valid input' do
+    input = StringIO.new('1')
+    ui = described_class.new(input, output)
+
+    expect(ui.run).to eq('1')
   end
 end
