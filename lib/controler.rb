64c9fe6d927d4170e9fc55ce857c94c3f449d1fd
MattW <mattnwarnock@yahoo.co.uk>
2020-09-15 15:05:58 +0100

    Setup first test

---
+++ lib/controler.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Controler
+  def initialize(user_interface)
+    @user_interface = user_interface
+  end
+
+  def start
+  end
+end
---
+++ spec/controler_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'controler'
+require 'user_interface'
+
+RSpec.describe Controler do
+  it 'tells the user interface to print the menu' do
+    input = StringIO.new('1')
+    output = StringIO.new
+    ui = UserInterface.new(input, output)
+    ctrl = Controler.new(ui)
+
+    ctrl.start
+
+    expect(output.string).to include(ui::MENU_MESSAGE)
+  end
+end
