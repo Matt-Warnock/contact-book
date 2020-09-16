f040161dd42a596a5a5d23d250b976beb555ac00
MattW <mattnwarnock@yahoo.co.uk>
2020-09-16 16:51:53 +0100

    pass final tests

--- spec/controler_spec.rb
+++ spec/controler_spec.rb
@@ -2,20 +2,21 @@
 
 require 'controler'
 require 'user_interface'
-require 'null_action'
 
 RSpec.describe Controler do
-  let(:valid_input) { StringIO.new('1') }
+  let(:actions) { [double('NullAction')] }
   let(:output) { StringIO.new }
-  let(:actions) { [NullAction.new] }
+  let(:valid_input) { StringIO.new('1') }
 
   it 'tells the user_interface to print the menu' do
     ui = UserInterface.new(valid_input, output)
     ctrl = Controler.new(ui, actions)
 
+    allow(actions[0]).to receive(:run)
+
     ctrl.start
 
-    expect(output.string).to include('CONTACT BOOK')
+    expect(output.string).to include(MENU_MESSAGE)
   end
 
   it 'receives the input from the user_interface' do
@@ -23,17 +24,43 @@ RSpec.describe Controler do
     ui = UserInterface.new(input, output)
     ctrl = Controler.new(ui, actions)
 
+    allow(actions[0]).to receive(:run)
+
     ctrl.start
 
-    expect(output.string).to include('Wrong input. Please try again: ')
+    expect(output.string).to include(ERROR_MESSAGE)
   end
 
   it 'selects correct action to perform according to input' do
     ui = UserInterface.new(valid_input, output)
     ctrl = Controler.new(ui, actions)
 
+    allow(actions[0]).to receive(:run)
+
+    expect(ctrl.start).to eq(nil)
+  end
+
+  it 'runs the action' do
+    ui = UserInterface.new(valid_input, output)
+    ctrl = Controler.new(ui, actions)
+
+    allow(actions[0]).to receive(:run)
+
     ctrl.start
 
-    expect(actions[0]).to be_called
+    expect(actions[0]).to have_received(:run)
   end
+
+  ERROR_MESSAGE = 'Wrong input. Please try again: '
+  MENU_MESSAGE = %{
+    ---------------------
+
+        CONTACT BOOK
+
+    ---------------------
+
+
+  1) Exit the program
+
+  Choose a menu option: }
 end
