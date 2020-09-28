98259fac46bb00561b0993b7e030a598bd4d8933
MattW <mattnwarnock@yahoo.co.uk>
2020-09-28 17:34:29 +0100

    Setup forth test

--- spec/creator_spec.rb
+++ spec/creator_spec.rb
@@ -5,7 +5,7 @@ require 'creator'
 RSpec.describe Creator do
   describe '#run' do
     let(:database) { double('ArrayDatabase', create: nil) }
-    let(:ui) { double('UserInterface', ask_for_fields: test_details, display: nil) }
+    let(:ui) { double('UserInterface', ask_for_fields: test_details, display: nil, add_another_contact?: false) }
 
     it 'tells the UI to ask for contact details' do
       creator = described_class.new(ui, database)
@@ -29,6 +29,14 @@ RSpec.describe Creator do
       creator.run
 
       expect(ui).to have_received(:display).with(test_details)
+    end
+
+    it 'tells the user interface to ask the user if they want to add another contact' do
+      creator = described_class.new(ui, database)
+
+      creator.run
+
+      expect(ui).to have_received(:add_another_contact?).once
     end
   end
 
