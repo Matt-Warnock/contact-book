71e3baecd37292511ab717258b5e8d8a10ab32d2
MattW <mattnwarnock@yahoo.co.uk>
2020-10-02 13:36:45 +0100

    Refactor contoler test back to smimiler previous state

--- spec/controler_spec.rb
+++ spec/controler_spec.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 require 'controler'
+require 'null_action'
 require 'user_interface'
 
 RSpec.describe Controler do
-  let(:controler) { described_class.new(ui, [creator, null_action]) }
-  let(:creator) { double('Creator', run: nil) }
+  let(:actions) { [NullAction.new, NullAction.new] }
   let(:exit_input) { StringIO.new('2') }
-  let(:null_action) { double('NullAction', run: nil) }
   let(:output) { StringIO.new }
-  let(:ui) { UserInterface.new(exit_input, output) }
 
   it 'tells the user interface to print the menu' do
+    ui = UserInterface.new(exit_input, output)
+    controler = described_class.new(ui, actions)
+
     controler.start
 
     expect(output.string).to include(UserInterface::MENU_MESSAGE)
@@ -19,23 +20,28 @@ RSpec.describe Controler do
 
   it 'receives the input from the user interface' do
     ui_double = double('UserInterface', menu_choice: 1)
-    controler = described_class.new(ui_double, [creator, null_action])
+    controler = described_class.new(ui_double, actions)
 
     controler.start
 
     expect(ui_double).to have_received(:menu_choice).twice
   end
 
-  it 'runs correct action to perform according to input' do
+  it 'runs the action chosen' do
+    null_action = double('NullAction', run: nil)
+    ui = UserInterface.new(exit_input, output)
+    controler = described_class.new(ui, [null_action, null_action])
+
     controler.start
 
     expect(null_action).to have_received(:run).once
   end
 
   it 'prints menu after action is run' do
+    null_action = double('NullAction', run: nil)
     add_contact_input = StringIO.new("1\n2")
     ui = UserInterface.new(add_contact_input, output)
-    controler = described_class.new(ui, [creator, null_action])
+    controler = described_class.new(ui, [null_action, null_action])
 
     controler.start
 
@@ -46,6 +52,9 @@ RSpec.describe Controler do
   end
 
   it 'does not print menu again if exit is chosen' do
+    ui = UserInterface.new(exit_input, output)
+    controler = described_class.new(ui, actions)
+
     controler.start
 
     expect(output.string).to eq(UserInterface::CLEAR_COMMAND + UserInterface::MENU_MESSAGE)
