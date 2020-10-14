6a65cfcf496495a1ef4bb402f049e77cf74f4317
MattW <mattnwarnock@yahoo.co.uk>
2020-10-14 16:34:24 +0100

    Setup sort test with refactoring

--- spec/pager_spec.rb
+++ spec/pager_spec.rb
@@ -6,37 +6,49 @@ require 'user_interface'
 require 'validator'
 
 RSpec.describe Pager do
+  let(:database) { ArrayDatabase.new }
+  let(:input) { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:pager) { Pager.new(user_interface, database) }
+  let(:user_interface) { UserInterface.new(input, output, validator) }
+  let(:validator) { Validator.new }
+
   describe '#run' do
     it 'prints a message to the user if the database is empty' do
-      database = ArrayDatabase.new
-      input = StringIO.new
-      output = StringIO.new
-      validator = Validator.new
-      user_interface = UserInterface.new(input, output, validator)
-      pager = Pager.new(user_interface, database)
-
       pager.run
 
       expect(output.string).to eq(UserInterface::NO_CONTACTS_MESSAGE)
     end
 
     it 'does not prints a message if the database has any contacts' do
-      database = ArrayDatabase.new
-      input = StringIO.new
-      output = StringIO.new
-      validator = Validator.new
-      user_interface = UserInterface.new(input, output, validator)
-      pager = Pager.new(user_interface, database)
-
-      database.create(test_details)
+      database.create(first_test_details)
 
       pager.run
 
       expect(output.string).to_not eq(UserInterface::NO_CONTACTS_MESSAGE)
     end
+
+    it 'sorts contacts into aphabetical order according to first name' do
+      database.create(first_test_details)
+      database.create(second_test_details)
+
+      pager.run
+
+      expect(database.all).to eq([second_test_details, first_test_details])
+    end
   end
 
-  def test_details
+  def first_test_details
+    {
+      name: 'Warran Smith',
+      address: 'Some address',
+      phone: '08796564231',
+      email: 'warran@damon.com',
+      notes: 'some guy I know'
+    }
+  end
+
+  def second_test_details
     {
       name: 'Matt Damon',
       address: 'Some address',
