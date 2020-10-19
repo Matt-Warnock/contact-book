36402c1cdcc254b8d2c00e433ac3cdafea724026
MattW <mattnwarnock@yahoo.co.uk>
2020-10-19 13:48:24 +0100

    fix page_all_contacts bug

--- lib/pager.rb
+++ lib/pager.rb
@@ -11,7 +11,7 @@ class Pager
       user_interface.display_no_contacts_message
     else
       alphabetize_contacts
-      user_interface.display_letter_header(database.all[0][:name].chr)
+      page_all_contacts
     end
   end
 
@@ -20,6 +20,17 @@ class Pager
   def alphabetize_contacts
     database.all
             .sort! { |a, b| a[:name] != b[:name] ? a[:name] <=> b[:name] : a[:email] <=> b[:email] }
+  end
+
+  def page_all_contacts
+    page_index = ''
+    database.all.each do |contact|
+      name_initial = contact[:name].chr
+
+      user_interface.display_letter_header(name_initial) unless name_initial == page_index
+      user_interface.display_contact(contact)
+      page_index = name_initial
+    end
   end
 
   attr_reader :database, :user_interface
