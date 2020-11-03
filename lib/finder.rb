b2b881f7391d6232c3658c4c0a00d5ef59183128
MattW <mattnwarnock@yahoo.co.uk>
2020-11-03 14:29:43 +0000

    Extract result actions to reduce cognitive complexity

--- lib/finder.rb
+++ lib/finder.rb
@@ -9,15 +9,17 @@ class Finder
   def run
     loop do
       search_result = database.search(user_interface.search_term)
-
-      user_interface.display_no_contacts_message unless search_result.any?
-      search_result.each { |contact| user_interface.display_contact(contact) }
-
+      display_results(search_result)
       break unless user_interface.search_again?
     end
   end
 
   private
+
+  def display_results(search_result)
+    user_interface.display_no_contacts_message unless search_result.any?
+    search_result.each { |contact| user_interface.display_contact(contact) }
+  end
 
   attr_reader :user_interface, :database
 end
